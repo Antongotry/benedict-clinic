0511b7be157ac9880938727883c234a58414e457
mac <mac@MacBook-Pro-mac.local>
2026-01-18 17:42:59 +0200

    Исправлена ошибка WP_CACHE: добавлена проверка перед определением константы

--- functions.php
+++ functions.php
@@ -294,7 +294,11 @@ add_filter('script_loader_src', 'benedict_asset_version', 10, 1);
 /**
  * Disable WordPress cache
  */
-define('WP_CACHE', false);/**
+if (!defined('WP_CACHE')) {
+    define('WP_CACHE', false);
+}
+
+/**
  * Force refresh CSS/JS on every load
  */
 function benedict_nocache_meta() {
