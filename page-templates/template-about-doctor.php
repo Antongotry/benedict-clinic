f3fb941e6db0267f74f5a20c2879d88d89d1acc6
mac <mac@MacBook-Pro-mac.local>
2026-03-02 13:27:05 +0200

    Update second podcast link on about-doctor page

--- page-templates/template-about-doctor.php
+++ page-templates/template-about-doctor.php
@@ -258,8 +258,8 @@ get_header();
             
             <div class="podcast-video-item">
                 <div class="podcast-video-thumbnail">
-                    <a href="https://www.youtube.com/@dr_benedikt" target="_blank" class="podcast-video-link">
-                        <img src="https://img.youtube.com/vi/dQw4w9WgXcQ/maxresdefault.jpg" alt="Чому чоловіки бояться урологів і як це ру...">
+                    <a href="https://www.youtube.com/watch?v=HcWsFe8CX3s&t=1324s" target="_blank" class="podcast-video-link">
+                        <img src="https://img.youtube.com/vi/HcWsFe8CX3s/maxresdefault.jpg" alt="Подкаст про урологічне здоров'я">
                         <div class="podcast-video-play-overlay">
                             <svg width="68" height="48" viewBox="0 0 68 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.63-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z" fill="#f00"/>
